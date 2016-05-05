ea5f6b51ba19efb13e2d8f8d0e56d473aa38697d
Jim Veneskey <github@strifecrafter.com>
2016-05-05 07:00:15 -0400

    Starting to add functionality to the buttons

--- README.md
+++ README.md
@@ -3,4 +3,6 @@
 Let off at:
 https://www.udemy.com/ios9-swift/learn/v4/t/lecture/3375308
 
+30:00 mark
+
 
--- retro-calculator/retro-calculator/ViewController.swift
+++ retro-calculator/retro-calculator/ViewController.swift
@@ -7,19 +7,71 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    enum Operation: String {
+        case Divide = "/"
+        case Multiply = "*"
+        case Subtract = "-"
+        case Add = "+"
+        case Equals = "="
+        case Empty = "Empty"
+    }
+    
+    @IBOutlet weak var outputLbl: UILabel!
+    
+    var btnSound: AVAudioPlayer!
+    
+    // accumulator
+    var runningNumber = ""
+    var leftValStr = ""
+    var rightValStr = ""
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // init the audio player
+        let path = NSBundle.mainBundle().pathForResource("btn", ofType: "wav")
+        let soundURL = NSURL(fileURLWithPath: path!)
+        do {
+            try btnSound = AVAudioPlayer(contentsOfURL: soundURL)
+            btnSound.prepareToPlay()
+        } catch let err as NSError {
+            print(err.debugDescription)
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func numberPressed(btn: UIButton!) {
+        btnSound.play()
     }
-
+    
+    @IBAction func onDividePressed(sender: AnyObject) {
+    
+    }
+    
+    @IBAction func onMultiplyPressed(sender: AnyObject) {
+    
+    }
+    
+    
+    @IBAction func onSubtractPressed(sender: AnyObject) {
+    
+    }
+    
+    @IBAction func onAddPressed(sender: AnyObject) {
+    
+    }
+    
+    
+    @IBAction func onEqualsPressed(sender: AnyObject) {
+    
+    }
+    
+    
 
 }
 
