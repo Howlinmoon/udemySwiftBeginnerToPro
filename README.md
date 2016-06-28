009affa51102be2b3b7073cb8d28466213a437ed
Jim Veneskey <github@strifecrafter.com>
2016-06-28 13:09:33 -0400

    Started working on app, ran into issue rounding corners on a button

--- README.md
+++ README.md
@@ -3,4 +3,8 @@
 Let off at:
 https://www.udemy.com/ios9-swift/learn/v4/t/lecture/3383182
 
-The Favorite Place App
+Let off at 10 minutes
+
+The Favorite Place Ap
+
+Problem with buttonp
---
+++ favorite-place/favorite-place/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  favorite-place
+//
+//  Created by jim Veneskey on 6/28/16.
+//  Copyright © 2016 Jim Veneskey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var moreBtn: UIButton!
+    
+    moreBtn.layer.cornerRadius = 2.0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
