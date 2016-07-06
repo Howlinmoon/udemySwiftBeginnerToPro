36d429b4dd587e763d95355540affa75716e8e48
Jim Veneskey <github@strifecrafter.com>
2016-07-06 13:19:22 -0400

    Now displaying 3 websites at the press of a button

--- README.md
+++ README.md
@@ -5,6 +5,6 @@ https://www.udemy.com/ios9-swift/learn/v4/t/lecture/3383258
 
 WKWebView
 
-Left off at: 07:42
+Left off at: 18:42
 
 
--- webrequest-test/webrequest-test/ViewController.swift
+++ webrequest-test/webrequest-test/ViewController.swift
@@ -23,11 +23,46 @@ class ViewController: UIViewController {
         
     }
 
+    override func viewDidAppear(animated: Bool) {
+        print("The width of the screen is \(container.bounds.width) believe it or don't!'")
+        // make the frame of the web view match its' container
+        let frame = CGRectMake(0, 0, container.bounds.width, container.bounds.height)
+        webView.frame = frame
+        
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func loadRequest(urlStr: String) {
+        
+        let url = NSURL(string: urlStr)!
+        let request = NSURLRequest(URL: url)
+        
+        webView.loadRequest(request)
+    }
 
+    @IBAction func loadSwift(sender: AnyObject) {
+        print("Loading Swift Blog")
+        loadRequest("https://developer.apple.com/swift/blog/")
+    }
 
+    
+    @IBAction func loadSteak(sender: AnyObject) {
+        print("Loading Google News")
+        loadRequest("https://news.google.com")
+    
+    }
+    
+    
+    @IBAction func loadRumors(sender: AnyObject) {
+        print("Loading bigorc.com")
+        loadRequest("https://www.bigorc.com/")
+    }
+    
+    
 }
 
