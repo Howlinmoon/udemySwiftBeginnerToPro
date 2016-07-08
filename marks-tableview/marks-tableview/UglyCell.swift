e0d7e2601af2f97a1eda2878ab6ccef222f6938c
Jim Veneskey <github@strifecrafter.com>
2016-07-08 12:53:50 -0400

    Handcrafting a table view

---
+++ marks-tableview/marks-tableview/UglyCell.swift
@@ -0,0 +1,27 @@
+//
+//  UglyCell.swift
+//  marks-tableview
+//
+//  Created by jim Veneskey on 7/8/16.
+//  Copyright © 2016 Jim Veneskey. All rights reserved.
+//
+
+import UIKit
+
+class UglyCell: UITableViewCell {
+
+    @IBOutlet weak var mainImg: UIImageView!
+    @IBOutlet weak var mainLbl: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    func configureCell (image:UIImage, text:String) {
+        mainImg.image = image
+        mainLbl.text = text
+    }
+
+
+}
---
+++ marks-tableview/marks-tableview/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  marks-tableview
+//
+//  Created by jim Veneskey on 7/8/16.
+//  Copyright © 2016 Jim Veneskey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var uglyThings = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("uglyCell")
+        return cell
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return uglyThings.count
+    }
+
+}
+
