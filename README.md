3427fd9623126bda0ebc0fc39ef8520fcc93d7bb
Jim Veneskey <github@strifecrafter.com>
2016-07-20 12:52:27 -0400

    Starting a scrollview tutorial

--- README.md
+++ README.md
@@ -3,6 +3,8 @@
 Let off at:
 https://www.udemy.com/ios9-swift/learn/v4/t/lecture/3383554
 
+10:20
+
 
 
 
---
+++ TheWalkingScrollView/TheWalkingScrollView/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TheWalkingScrollView
+//
+//  Created by jim Veneskey on 7/20/16.
+//  Copyright © 2016 Jim Veneskey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    let WIDTH: CGFloat = 240
+    let HEIGHT: CGFloat = 397
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        
+    }
+
+
+
+}
+
